de6be924f4264bbd5c86cc646aaf7ecb84f29451
JoelCruzCode <136981207+JoelCruzCode@users.noreply.github.com>
2023-07-13 18:52:22 -0700

    Implement changeGrid function

--- script.js
+++ script.js
@@ -4,16 +4,24 @@
 // create hover affect
 // create button for colors
 
+let color;
+let grid;
+let gridValue;
+let columns;
 const container = document.querySelector(".container");
 const blackBtn = document.querySelector(".black-btn");
 const blueBtn = document.querySelector(".blue-btn");
-let color;
-let grid;
+const gridBtn1 = document.querySelector(".grid-btn-1");
+const gridBtn2 = document.querySelector(".grid-btn-2");
+const gridBtn3 = document.querySelector(".grid-btn-3");
+const resetBtn = document.querySelector(".reset-btn");
 
-const createGrids = function (gridValue = 20 * 20) {
+const createGrids = function (gridValue = 20 * 20, columns = 20) {
   for (i = 0; i < gridValue; i++) {
+    console.log(gridValue);
     grid = document.createElement("div");
     grid.classList.add("grid");
+    container.style.gridTemplateColumns = `repeat(${columns}, auto)`;
     container.appendChild(grid);
   }
 };
@@ -23,6 +31,13 @@ const colorPicker = function () {
   console.log(color);
 };
 
+const changeGrid = function () {
+  container.innerHTML = ``;
+  columns = Number(this.textContent.split(" ")[0]);
+  gridValue = columns * columns;
+  console.log((gridValue, columns));
+  createGrids(gridValue, columns);
+};
 createGrids();
 
 container.addEventListener("mouseover", function (e) {
@@ -31,3 +46,7 @@ container.addEventListener("mouseover", function (e) {
 
 blackBtn.addEventListener("click", colorPicker);
 blueBtn.addEventListener("click", colorPicker);
+resetBtn.addEventListener("click", changeGrid);
+gridBtn1.addEventListener("click", changeGrid);
+gridBtn2.addEventListener("click", changeGrid);
+gridBtn3.addEventListener("click", changeGrid);
