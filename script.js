256567ab3e7300b054e4d11a10a1921549b456b9
JoelCruzCode <136981207+JoelCruzCode@users.noreply.github.com>
2023-07-13 19:24:21 -0700

    Clean up code with dry principle

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-// dynamically change height to adjust to number of grids
-// create a button to choose grid number,
-// create reset button
-// create hover affect
-// create button for colors
-
 let color;
-let grid;
 let gridValue;
 let columns;
 const container = document.querySelector(".container");
-const blackBtn = document.querySelector(".black-btn");
-const blueBtn = document.querySelector(".blue-btn");
-const gridBtn1 = document.querySelector(".grid-btn-1");
-const gridBtn2 = document.querySelector(".grid-btn-2");
-const gridBtn3 = document.querySelector(".grid-btn-3");
+const colorBtns = document.querySelectorAll(".color-btn");
+const gridBtns = document.querySelectorAll(".grid-btn");
 const resetBtn = document.querySelector(".reset-btn");
 
 const createGrids = function (gridValue = 20 * 20, columns = 20) {
   for (i = 0; i < gridValue; i++) {
-    console.log(gridValue);
-    grid = document.createElement("div");
+    let grid = document.createElement("div");
     grid.classList.add("grid");
     container.style.gridTemplateColumns = `repeat(${columns}, auto)`;
     container.appendChild(grid);
@@ -28,14 +17,12 @@ const createGrids = function (gridValue = 20 * 20, columns = 20) {
 
 const colorPicker = function () {
   color = this.textContent;
-  console.log(color);
 };
 
 const changeGrid = function () {
   container.innerHTML = ``;
   columns = Number(this.textContent.split(" ")[0]);
   gridValue = columns * columns;
-  console.log((gridValue, columns));
   createGrids(gridValue, columns);
 };
 
@@ -48,11 +35,8 @@ container.addEventListener("mouseover", function (e) {
   e.target.style.backgroundColor = color;
 });
 
-blackBtn.addEventListener("click", colorPicker);
-blueBtn.addEventListener("click", colorPicker);
+colorBtns.forEach((btn) => btn.addEventListener("click", colorPicker));
+gridBtns.forEach((btn) => btn.addEventListener("click", changeGrid));
 resetBtn.addEventListener("click", resetGrid);
-gridBtn1.addEventListener("click", changeGrid);
-gridBtn2.addEventListener("click", changeGrid);
-gridBtn3.addEventListener("click", changeGrid);
 
 createGrids();
